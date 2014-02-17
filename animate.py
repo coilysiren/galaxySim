787b93e1178265c8d31340ebf53f5de13507782f
test <lynn@ubuntu.(none)>
2014-02-17 02:18:20 -0800

    gif-eration operational

--- animate.py
+++ animate.py
@@ -1,55 +1,46 @@
 #animate.py
 
-from __future__ import division
 import time
 import numpy
+import PIL
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import os
-
-def plotter ():
-	plt.ion()
-	npframes = numpy.load("output.npy")
-	xs = range(npframes[0].shape[0]+1)
-	ys = range(npframes[0].shape[1]+1)
-	X,Y = numpy.meshgrid(xs,ys)
-	img_list = plt.pcolormesh(X,Y,npframes[0],cmap="hot")
-	for i in range(1,len(npframes)):
-	    img_list.set_array(npframes[i].ravel())
-	    img_list.autoscale()
-	    plt.savefig("figure"+str(i)+".png")
-	    plt.draw()
-	    time.sleep(0.025)
-
-def imager ():
-	plt.ion()
-	npframes = numpy.load("output.npy")
-	xs = range(npframes[0].shape[0]+1)
-	ys = range(npframes[0].shape[1]+1)
-	X,Y = numpy.meshgrid(xs,ys)
-	img_list = plt.pcolormesh(X,Y,npframes[0],cmap="hot")
-	for i in range(1,len(npframes)):
-	    img_list.set_array(npframes[i].ravel())
-	    img_list.autoscale()
-	    plt.savefig("figure"+str(i)+".png")
+import images2gif
 
 def giffer ():
-	'''make gif'''
-	#deps
-	__author__ = 'Robert'
-	from images2gif import writeGif
-	from PIL import Image
-	imager()
-	#code
-	file_names = sorted([fn for fn in os.listdir('.') if fn.endswith('.png')])
-	print(file_names)
-	images = [Image.open(fn) for fn in file_names]
-	filename = "plot_gif.gif"
-	writeGif(filename, images, duration=0.2)
-	
+    plt.ion()
+    npframes = numpy.load("output.npy")
+    xs = range(npframes[0].shape[0]+1)
+    ys = range(npframes[0].shape[1]+1)
+    X,Y = numpy.meshgrid(xs,ys)
+    dataList = plt.pcolormesh(X,Y,npframes[0],cmap="hot")
+    images = list()
+    for i in range(1,len(npframes)):
+        thisFig = "figure"+str(i)+".jpg"
+        dataList.set_array(npframes[i].ravel())
+        dataList.autoscale()
+        plt.savefig(thisFig)
+        images.append(PIL.Image.open(thisFig))
+        os.remove(thisFig)
+    animName = "plot_gif.gif"
+    images2gif.writeGif(filename=animName, images=images, duration=0.2, repeat=False)
+
+def plotter ():
+    plt.ion()
+    npframes = numpy.load("output.npy")
+    xs = range(npframes[0].shape[0]+1)
+    ys = range(npframes[0].shape[1]+1)
+    X,Y = numpy.meshgrid(xs,ys)
+    img_list = plt.pcolormesh(X,Y,npframes[0],cmap="hot")
+    for i in range(1,len(npframes)):
+        img_list.set_array(npframes[i].ravel())
+        img_list.autoscale()
+        plt.draw()
+        time.sleep(0.025)
+    
 
 if __name__ == "__main__":
-	#plotter()
-	#imager()
-	giffer()
+    plotter()
+    #giffer()
